16de24ee7647bd75194c350df668b02edbe47404
Escuela de Oficios Electrosonoros <gurrupibaby@gmail.com>
2022-06-18 20:12:50 +0200

    add literatura

--- imagenes/js/literatura.js
+++ imagenes/js/literatura.js
@@ -7,44 +7,44 @@ var yellowIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
-var titulo1 = 'Paisaje sonoro basado en el relato <i>Desde hacía algún tiempo</i>.';
-var url1 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/Desde-hace-un-tiempo.mp3';
-var markerAudio1 = L.marker([40.86,-3.61548], {icon: yellowIcon})
+var titulo1 = 'Lectura del relato <i>Desde hace algún tiempo</i>.';
+var url1 = 'https://archive.org/download/steam-con-a-literatura/Desde-hace-un-tiempo.mp3';
+var markerAudio1 = L.marker([40.858735, -3.618612], {icon: yellowIcon})
 .bindPopup('<h3>'+titulo1+'</h3>'+'<audio controls><source src="'+url1+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
-var titulo2 = 'Paisaje sonoro basado en el relato <i>Había pasado un mes</i>.';
-var url2 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/Habia-pasado-un-mes.mp3';
-var markerAudio2 = L.marker([40.850648, -3.627969], {icon: yellowIcon})
+var titulo2 = 'Lectura del relato <i>El hombre es el Lobo del hombre</i>.';
+var url2 = 'https://archive.org/download/steam-con-a-literatura/El-hombre-es-el-lobo-del-hombre.mp3';
+var markerAudio2 = L.marker([40.853965, -3.616822], {icon: yellowIcon})
 .bindPopup('<h3>'+titulo2+'</h3><audio controls><source src="'+url2+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
-var titulo3 = 'Paisaje sonoro basado en el relato <i>Desperté otro día más</i>.';
-var url3 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%203.mp3';
-var markerAudio3 = L.marker([40.838409, -3.636872], {icon: yellowIcon})
+var titulo3 = 'Lectura del relato <i>Había pasado un mes</i>.';
+var url3 = 'https://archive.org/download/steam-con-a-literatura/Habia-pasado-un-mes.mp3';
+var markerAudio3 = L.marker([40.850565, -3.629364], {icon: yellowIcon})
 .bindPopup('<h3>'+titulo3+'</h3><audio controls><source src="'+url3+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
-var titulo4 = 'Paisaje sonoro basado en el relato <i>El Hombre es el Lobo del Hombre</i>.';
-var url4 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%204.mp3';
-var markerAudio4 = L.marker([40.826265, -3.633525], {icon: yellowIcon})
+var titulo4 = 'Lectura del relato <i>Me desperté otro día más</i>.';
+var url4 = 'https://archive.org/download/steam-con-a-literatura/Me-desperte-otro-dia-mas.mp3';
+var markerAudio4 = L.marker([40.845079, -3.633913], {icon: yellowIcon})
 .bindPopup('<h3>'+titulo4+'</h3><audio controls><source src="'+url4+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
-var titulo5 = 'Paisaje sonoro basado en el relato <i>Desperté otro día más</i>.';
-var url5 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%203.mp3';
-var markerAudio3 = L.marker([40.838409, -3.636872], {icon: yellowIcon})
-.bindPopup('<h3>'+titulo3+'</h3><audio controls><source src="'+url3+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
+var titulo5 = 'Lectura del relato <i>Me llamo Ben</i>.';
+var url5 = 'https://archive.org/download/steam-con-a-literatura/Me-llamo-Ben.mp3';
+var markerAudio3 = L.marker([40.838634, -3.636563], {icon: yellowIcon})
+.bindPopup('<h3>'+titulo5+'</h3><audio controls><source src="'+url5+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
-var titulo6 = 'Paisaje sonoro basado en el relato <i>El Hombre es el Lobo del Hombre</i>.';
-var url6 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%204.mp3';
-var markerAudio4 = L.marker([40.826265, -3.633525], {icon: yellowIcon})
-.bindPopup('<h3>'+titulo4+'</h3><audio controls><source src="'+url4+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
+var titulo6 = 'Lectura del relato <i>Tras la crisis</i>.';
+var url6 = 'https://archive.org/download/steam-con-a-literatura/Tras-la-crisis.mp3';
+var markerAudio4 = L.marker([40.831483, -3.638601], {icon: yellowIcon})
+.bindPopup('<h3>'+titulo6+'</h3><audio controls><source src="'+url6+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
--- imagenes/js/musica.js
+++ imagenes/js/musica.js
@@ -16,21 +16,21 @@ var markerAudio1 = L.marker([40.86,-3.61548], {icon: greenIcon})
 
 var titulo2 = 'Paisaje sonoro basado en el relato <i>Había pasado un mes</i>.';
 var url2 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/Habia-pasado-un-mes.mp3';
-var markerAudio2 = L.marker([40.850648, -3.627969], {icon: greenIcon})
+var markerAudio2 = L.marker([40.850648, -3.623969], {icon: greenIcon})
 .bindPopup('<h3>'+titulo2+'</h3><audio controls><source src="'+url2+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
 var titulo3 = 'Paisaje sonoro basado en el relato <i>Desperté otro día más</i>.';
 var url3 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%203.mp3';
-var markerAudio3 = L.marker([40.838409, -3.636872], {icon: greenIcon})
+var markerAudio3 = L.marker([40.838409, -3.631872], {icon: greenIcon})
 .bindPopup('<h3>'+titulo3+'</h3><audio controls><source src="'+url3+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
 var titulo4 = 'Paisaje sonoro basado en el relato <i>El Hombre es el Lobo del Hombre</i>.';
 var url4 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%204.mp3';
-var markerAudio4 = L.marker([40.826265, -3.633525], {icon: greenIcon})
+var markerAudio4 = L.marker([40.826265, -3.626525], {icon: greenIcon})
 .bindPopup('<h3>'+titulo4+'</h3><audio controls><source src="'+url4+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
