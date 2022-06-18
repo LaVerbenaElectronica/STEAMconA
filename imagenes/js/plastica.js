4cab43457440663c084b86c7929b3b47888dba19
Escuela de Oficios Electrosonoros <gurrupibaby@gmail.com>
2022-06-18 11:26:31 +0200

    update map cabrera

--- imagenes/js/plastica.js
+++ imagenes/js/plastica.js
@@ -1,9 +1,127 @@
-var imageUrl = 'imagenes/plastica/ar1.png';
-var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
-var altText = 'Imagen elaborada por Adolfo Rodríguez García';
-var latLngBounds = L.latLngBounds([[40.86619,-3.61548], [40.86719,-3.61648]]);
 
-var imageOverlay = L.imageOverlay(imageUrl, latLngBounds, {
+//adolfo-rodriguez
+
+var ar1 = 'imagenes/plastica/adolfo-rodriguez/ar1.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 1 elaborada por Adolfo Rodríguez García';
+var latLngBounds = L.latLngBounds([[40.86619,-3.61548], [40.86719,-3.61710]]);
+var imageOverlay = L.imageOverlay(ar1, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var ar2 = 'imagenes/plastica/adolfo-rodriguez/ar2.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 2 elaborada por Adolfo Rodríguez García';
+var latLngBounds = L.latLngBounds([[40.85619,-3.61548], [40.85719,-3.61645]]);
+var imageOverlay = L.imageOverlay(ar2, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var ar3 = 'imagenes/plastica/adolfo-rodriguez/ar3.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 3 elaborada por Adolfo Rodríguez García';
+var latLngBounds = L.latLngBounds([[40.85919,-3.61548], [40.86019,-3.61710]]);
+var imageOverlay = L.imageOverlay(ar3, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+//aitor-del-primo
+
+var ap1 = 'imagenes/plastica/aitor-del-primo/ap1.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 1 elaborada por Aitor del Primo Sánchez';
+var latLngBounds = L.latLngBounds([[40.83999,-3.63542], [40.84099,-3.63642]]);
+var imageOverlay = L.imageOverlay(ap1, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var ap2 = 'imagenes/plastica/aitor-del-primo/ap2.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 2 elaborada por Aitor del Primo Sánchez';
+var latLngBounds = L.latLngBounds([[40.85904,-3.61981], [40.85804,-3.61881]]);
+var imageOverlay = L.imageOverlay(ap2, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var ap3 = 'imagenes/plastica/aitor-del-primo/ap3.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 3 elaborada por Aitor del Primo  Sánchez';
+var latLngBounds = L.latLngBounds([[40.85950,-3.61938], [40.86050,-3.61838]]);
+var imageOverlay = L.imageOverlay(ap3, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+//beltran-acevedo
+
+var ba1 = 'imagenes/plastica/beltran-acevedo/ba1.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 1 elaborada por Beltrán Acevedo de Miguel';
+var latLngBounds = L.latLngBounds([[40.85400,-3.62016], [40.85500,-3.62116]]);
+var imageOverlay = L.imageOverlay(ba1, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var ba2 = 'imagenes/plastica/beltran-acevedo/ba2.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 2 elaborada por Beltrán Acevedo de Miguel';
+var latLngBounds = L.latLngBounds([[40.85450,-3.61816], [40.85550,-3.61916]]);
+var imageOverlay = L.imageOverlay(ba2, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var ba3 = 'imagenes/plastica/beltran-acevedo/ba3.png';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 3 elaborada por Beltrán Acevedo de Miguel';
+var latLngBounds = L.latLngBounds([[40.85550,-3.61833], [40.85650,-3.61933]]);
+var imageOverlay = L.imageOverlay(ba3, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+//claudia-santamaria
+
+var cs1 = 'imagenes/plastica/claudia-santamaria/cs1.jpg';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 1 elaborada por Claudia María Santamaría';
+var latLngBounds = L.latLngBounds([[40.85600,-3.62016], [40.85700,-3.62116]]);
+var imageOverlay = L.imageOverlay(cs1, latLngBounds, {
+    opacity: 0.8,
+    errorOverlayUrl: errorOverlayUrl,
+    alt: altText,
+    interactive: true
+}).addTo(map).bindPopup(altText);
+
+var cs2 = 'imagenes/plastica/claudia-santamaria/cs2.jpg';
+var errorOverlayUrl = 'https://cdn-icons-png.flaticon.com/512/110/110686.png';
+var altText = 'Imagen 2 elaborada por Claudia María Santamaría';
+var latLngBounds = L.latLngBounds([[40.85650,-3.61816], [40.85750,-3.61916]]);
+var imageOverlay = L.imageOverlay(cs2, latLngBounds, {
     opacity: 0.8,
     errorOverlayUrl: errorOverlayUrl,
     alt: altText,
