76bb83097bf8e262313e9b41d8ee942adff265e3
Escuela de Oficios Electrosonoros <gurrupibaby@gmail.com>
2022-06-19 11:37:57 +0200

    actualizar audios

--- imagenes/js/musica.js
+++ imagenes/js/musica.js
@@ -9,28 +9,42 @@ var greenIcon = new L.Icon({
 
 var titulo1 = 'Paisaje sonoro basado en el relato <i>Desde hacía algún tiempo</i>.';
 var url1 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/Desde-hace-un-tiempo.mp3';
-var markerAudio1 = L.marker([40.86,-3.61548], {icon: greenIcon})
+var markerAudio1 = L.marker([40.858735, -3.617612], {icon: greenIcon})
 .bindPopup('<h3>'+titulo1+'</h3>'+'<audio controls><source src="'+url1+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
 var titulo2 = 'Paisaje sonoro basado en el relato <i>Había pasado un mes</i>.';
 var url2 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/Habia-pasado-un-mes.mp3';
-var markerAudio2 = L.marker([40.850648, -3.623969], {icon: greenIcon})
+var markerAudio2 = L.marker([40.850565, -3.628364], {icon: greenIcon})
 .bindPopup('<h3>'+titulo2+'</h3><audio controls><source src="'+url2+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
-var titulo3 = 'Paisaje sonoro basado en el relato <i>Desperté otro día más</i>.';
+var titulo3 = 'Paisaje sonoro basado en el relato <i>Desperté otro día</i>.';
 var url3 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%203.mp3';
-var markerAudio3 = L.marker([40.838409, -3.631872], {icon: greenIcon})
+var markerAudio3 = L.marker([40.845079, -3.632913], {icon: greenIcon})
 .bindPopup('<h3>'+titulo3+'</h3><audio controls><source src="'+url3+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
 
 var titulo4 = 'Paisaje sonoro basado en el relato <i>El Hombre es el Lobo del Hombre</i>.';
 var url4 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%204.mp3';
-var markerAudio4 = L.marker([40.826265, -3.626525], {icon: greenIcon})
+var markerAudio4 = L.marker([40.853965, -3.615822], {icon: greenIcon})
 .bindPopup('<h3>'+titulo4+'</h3><audio controls><source src="'+url4+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
+.openPopup()
+.addTo(map);
+
+var titulo5 = 'Paisaje sonoro basado en el relato <i>Me llamo Ben</i>.';
+var url5 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato5.me%20llamo%20ben...mp3';
+var markerAudio3 = L.marker([40.838634, -3.635563], {icon: greenIcon})
+.bindPopup('<h3>'+titulo5+'</h3><audio controls><source src="'+url5+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
+.openPopup()
+.addTo(map);
+
+var titulo6 = 'Paisaje sonoro basado en el relato <i>Tras la crisis</i>.';
+var url6 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%206.%20tras%20la%20crisis.mp3';
+var markerAudio4 = L.marker([40.831483, -3.637601], {icon: greenIcon})
+.bindPopup('<h3>'+titulo6+'</h3><audio controls><source src="'+url6+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
