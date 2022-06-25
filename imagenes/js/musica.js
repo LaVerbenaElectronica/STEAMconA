c9555629c5ac634b8dae0bcf577322d3a741e56f
Escuela de Oficios Electrosonoros <gurrupibaby@gmail.com>
2022-06-25 21:32:41 +0200

    add entrevista

--- imagenes/js/musica.js
+++ imagenes/js/musica.js
@@ -7,6 +7,15 @@ var greenIcon = new L.Icon({
   shadowSize: [41, 41]
 });
 
+var blackIcon = new L.Icon({
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 var titulo1 = 'Paisaje sonoro basado en el relato <i>Desde hacía algún tiempo</i>.';
 var url1 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/Desde-hace-un-tiempo.mp3';
 var markerAudio1 = L.marker([40.858735, -3.617612], {icon: greenIcon})
@@ -46,5 +55,12 @@ var titulo6 = 'Paisaje sonoro basado en el relato <i>Tras la crisis</i>.';
 var url6 = 'https://archive.org/download/steam-con-a-ies-la-cabrera/relato%206.%20tras%20la%20crisis.mp3';
 var markerAudio4 = L.marker([40.831483, -3.637601], {icon: greenIcon})
 .bindPopup('<h3>'+titulo6+'</h3><audio controls><source src="'+url6+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
+.openPopup()
+.addTo(map);
+
+var titulo7 = 'Entrevista realizada a alumnado de 1º ESO del IES la Cabrera';
+var url7 = 'https://archive.org/download/steam-con-a-literatura/entrevistas-ies-la-cabrera.mp3';
+var markerAudio4 = L.marker([40.867584, -3.609892], {icon: blackIcon})
+.bindPopup('<h3>'+titulo7+'</h3><audio controls><source src="'+url7+'" type="audio/mp3">Tu navegador no soporta audio.</audio>')
 .openPopup()
 .addTo(map);
